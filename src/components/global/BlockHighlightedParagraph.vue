81aeb9177ec9c13b22a0a3d2581ccf4365793efc
Lorenzo Girardi <lorenzo.girardi93@gmail.com>
2025-07-04 18:51:22 +0200

    feat: grid and bigger text on BlockHighlightedParagraph

--- src/components/global/BlockHighlightedParagraph.vue
+++ src/components/global/BlockHighlightedParagraph.vue
@@ -7,23 +7,25 @@
       'lightgrey-bg-b4': evidence
     }"
   >
-    <div class="row">
-      <div class="col-12">
-        <h2 :id="`title-${uid}`" class="primary-color-a9 mb-3">{{ title }}</h2>
-        <div v-if="htmlContent" class="text-md-left" v-html="htmlContent" />
-        <Button
-          v-if="buttonLink"
-          :download="isAsset ? true : undefined"
-          :href="!isNuxtLink ? href : undefined"
-          :rel="rel"
-          :tag="tagComponent"
-          :target="target"
-          :to="isNuxtLink ? to : undefined"
-        >
-          <slot>
-            {{ label }}
-          </slot>
-        </Button>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2 :id="`title-${uid}`" class="primary-color-a9 mb-3">{{ title }}</h2>
+          <div v-if="htmlContent" class="text-md-left" v-html="htmlContent" />
+          <Button
+            v-if="buttonLink"
+            :download="isAsset ? true : undefined"
+            :href="!isNuxtLink ? href : undefined"
+            :rel="rel"
+            :tag="tagComponent"
+            :target="target"
+            :to="isNuxtLink ? to : undefined"
+          >
+            <slot>
+              {{ label }}
+            </slot>
+          </Button>
+        </div>
       </div>
     </div>
   </Section>
@@ -66,3 +68,10 @@ const {
   isAsset
 } = useCraftLink(props.buttonLink)
 </script>
+
+<style scoped>
+.text-md-left :deep(p) {
+  font-size: 1.5rem;
+  line-height: 1.5;
+}
+</style>
